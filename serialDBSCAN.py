2975c0c96406fd8b93ac37625c9e795cd0f83272
Rodrigo <124520560+rlombamoreno@users.noreply.github.com>
2025-10-07 10:38:01 +0200

    change6-7/10/25

--- serialDBSCAN.py
+++ serialDBSCAN.py
@@ -7,13 +7,23 @@ import time
 import os
 
 def load_image():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("serialDBSCAN: Must specify one image filename")
         print("example: python3 serialDBSCAN filename.jpg")
         sys.exit(1)
     image_filename = sys.argv[1]
     image_orig = Image.open(image_filename)
     return np.array(image_orig.convert('1'), dtype=int)
+
+def load_std_scale():
+    if len(sys.argv) != 3:
+        print("serialDBSCAN: Using default std_scale=1")
+        return 1
+    std_scale = float(sys.argv[2])
+    if std_scale < 0 or std_scale > 1:
+        print("serialDBSCAN: std_scale must be between 0 and 1")
+        sys.exit(1)
+    return std_scale
 
 def compute_histogram(image):
     y_len, x_len = image.shape
@@ -23,37 +33,78 @@ def compute_histogram(image):
 @njit
 def get_points_in_cluster(image, color_marker):
     y_len, x_len = image.shape
-    count = 0
+    count = count_cluster_points(image, color_marker, y_len, x_len)
+    points = np.zeros((count, 2), dtype=np.int64)
+    point_index = 0
     for y in range(y_len):
         for x in range(x_len):
             if image[y, x] != color_marker:
-                count += 1
-    points = np.empty((count, 2), dtype=np.int64)
-    idx = 0
-    for y in range(y_len):
-        for x in range(x_len):
-            if image[y, x] != color_marker:
-                points[idx, 0] = x
-                points[idx, 1] = y
-                idx += 1
+                points[point_index, 0] = x
+                points[point_index, 1] = y
+                point_index += 1
     return points
 
 @njit
+def count_cluster_points(image, color_marker, y_len, x_len):
+    count = 0
+    for y in range(y_len):
+        for x in range(x_len):
+            if image[y, x] != color_marker:
+                count += 1
+    return count
+
+@njit
+def dbscan_core(points, eps, min_pts, labels):
+    cluster_id = 0
+    for point_index in range(len(points)):
+        if labels[point_index] == -1:
+            neighbors = find_neighbors(point_index, points, eps)
+            if len(neighbors)+1 >= min_pts: # +1 to include the point itself
+                cluster_id += 1
+                labels[point_index] = cluster_id
+                i = 0
+                while i < len(neighbors):
+                    neighbor_index = neighbors[i]
+                    if labels[neighbor_index] == -1:
+                        labels[neighbor_index] = cluster_id
+                        neighbors_aux = find_neighbors(neighbor_index, points, eps)
+                        if len(neighbors_aux)+1 >= min_pts:
+                            neighbors = append_neighbors(neighbors, neighbors_aux)
+                    if labels[neighbor_index] == 0:
+                        labels[neighbor_index] = cluster_id
+                    i += 1
+            else:
+                labels[point_index] = 0
+    return labels, cluster_id
+
+def dbscan(points, eps, min_pts):
+    labels = np.zeros(len(points), dtype=np.int64) - 1
+    labels, cluster_count= dbscan_core(points, eps, min_pts, labels)
+    clusters = []
+    for cluster_id in range(1, np.max(labels)+1):
+        cluster_points = points[labels == cluster_id]
+        if len(cluster_points) > 0:
+            clusters.append(cluster_points)
+    return clusters, labels
+
+@njit
 def find_neighbors(point_index, points, eps):
+    # Primera pasada: contar vecinos
     count = 0
     for i in range(len(points)):
         if i != point_index:
-            dx = points[point_index, 0] - points[i, 0]
-            dy = points[point_index, 1] - points[i, 1]
+            dx = points[point_index][0] - points[i][0]
+            dy = points[point_index][1] - points[i][1]
             distance = np.sqrt(dx * dx + dy * dy)
             if distance <= eps:
                 count += 1
+    # Segunda pasada: guardar vecinos
     neighbors = np.empty(count, dtype=np.int64)
     idx = 0
     for i in range(len(points)):
         if i != point_index:
-            dx = points[point_index, 0] - points[i, 0]
-            dy = points[point_index, 1] - points[i, 1]
+            dx = points[point_index][0] - points[i][0]
+            dy = points[point_index][1] - points[i][1]
             distance = np.sqrt(dx * dx + dy * dy)
             if distance <= eps:
                 neighbors[idx] = i
@@ -62,16 +113,21 @@ def find_neighbors(point_index, points, eps):
 
 @njit
 def append_neighbors(neighbors, neighbors_aux):
-    # Concatenate unique elements from neighbors_aux to neighbors
+    temp = np.empty(len(neighbors) + len(neighbors_aux), dtype=np.int64)
+    count = 0
+    for i in range(len(neighbors)):
+        temp[count] = neighbors[i]
+        count += 1
     for i in range(len(neighbors_aux)):
         found = False
-        for j in range(len(neighbors)):
+        for j in range(len(neighbors)):  
             if neighbors_aux[i] == neighbors[j]:
                 found = True
                 break
         if not found:
-            neighbors = np.append(neighbors, neighbors_aux[i])
-    return neighbors
+            temp[count] = neighbors_aux[i]
+            count += 1
+    return temp[:count]
 
 @njit
 def compute_kn_distances(points, k):
@@ -81,48 +137,20 @@ def compute_kn_distances(points, k):
         idx = 0
         for j in range(len(points)):
             if i != j:
-                dx = points[i, 0] - points[j, 0]
-                dy = points[i, 1] - points[j, 1]
+                dx = points[i][0] - points[j][0]
+                dy = points[i][1] - points[j][1]
                 eucl_dist[idx] = np.sqrt(dx * dx + dy * dy)
                 idx += 1
         eucl_dist.sort()
         kn_distances[i] = eucl_dist[k-1]
     return kn_distances
 
-def get_epsilon(points, k):
+def get_epsilon(points, k,std_scale):
     kn_distances = compute_kn_distances(points, k)
-    kn_distances = np.sort(kn_distances)[::-1]
-    diffs = np.diff(kn_distances)
-    elbow_index = np.argmax(diffs)
-    epsilon = kn_distances[elbow_index]
+    kn_distances = np.sort(kn_distances)[::1]
+    epsilon = np.mean(kn_distances) + np.std(kn_distances) * std_scale
     print(f"Recommended epsilon (at elbow): {epsilon}")
     return epsilon
-
-def dbscan(points, eps, min_pts):
-    labels = np.zeros(len(points), dtype=int) - 1
-    cluster_id = 0
-    clusters = []
-    for point_index in range(len(points)):
-        if labels[point_index] == -1:
-            neighbors = find_neighbors(point_index, points, eps)
-            if len(neighbors) >= min_pts:
-                labels[point_index] = cluster_id
-                cluster = [tuple(points[point_index])]
-                for neighbor_index in neighbors:
-                    if labels[neighbor_index] == -1:
-                        labels[neighbor_index] = cluster_id
-                        cluster.append(tuple(points[neighbor_index]))
-                        neighbors_aux = find_neighbors(neighbor_index, points, eps)
-                        if len(neighbors_aux) >= min_pts:
-                            neighbors = append_neighbors(neighbors, neighbors_aux)
-                    if labels[neighbor_index] == 0:
-                        labels[neighbor_index] = cluster_id
-                        cluster.append(tuple(points[neighbor_index]))
-                clusters.append(cluster)
-                cluster_id += 1
-            else:
-                labels[point_index] = 0
-    return clusters, labels
 
 def paint_clusters(image, clusters):
     image_colored = np.zeros((*image.shape, 3), dtype=np.uint8)
@@ -137,14 +165,15 @@ def paint_clusters(image, clusters):
 if __name__ == "__main__":
     start = time.time()
     image_bw = load_image()
-    print(f"Image shape: {image_bw.shape}")
+    std_scale=load_std_scale()
     hist = compute_histogram(image_bw)
     color_marker = 1 if hist[0] < hist[1] else 0
     points = get_points_in_cluster(image_bw, color_marker)
-    eps = get_epsilon(points, k=5)
-    t1 = time.time()
-    print("Time1 = ", t1 - start)
+    eps = get_epsilon(points, k=5,std_scale=std_scale)
+    timeEpsilon = time.time()
+    print("Time1 = ", timeEpsilon - start)
     clusters, labels = dbscan(points, eps, min_pts=5)
+    print(f"Number of clusters found: {len(clusters)}")
     end = time.time()
     print("Final time = ", end - start)
     image_colored = paint_clusters(image_bw, clusters)
@@ -155,6 +184,6 @@ if __name__ == "__main__":
     output_filename = f"{name}_clusters.png"
     plt.imsave(output_filename, image_colored)
     print(f"Clustered image saved as: {output_filename}")
-
+    
     plt.imshow(image_colored)
     plt.show()
