47c462d0ad0807f5bcc0fc989347523c16b1edda
Rodrigo <124520560+rlombamoreno@users.noreply.github.com>
2025-10-14 15:36:23 +0200

    change12-14/10/25

--- paralelDBSCAN.py
+++ paralelDBSCAN.py
@@ -56,7 +56,7 @@ def get_points_in_cluster(image, color_marker):
     
     kernel_code = r'''
     extern "C" __global__
-    void get_points_in_cluster(const int *image, int *points, int color_marker, int y_len, int x_len, int *points_index) {
+    void get_points_in_cluster(const int *image, int *points, const int color_marker, const int y_len, const int x_len, int *points_index) {
         int idx = blockIdx.x * blockDim.x + threadIdx.x;
         if (idx < y_len * x_len) {
             int x = idx % x_len;
@@ -90,7 +90,7 @@ def count_cluster_points(image, color_marker, y_len, x_len):
     
     kernel_code =  r'''
     extern "C" __global__
-    void count_points(const int *image, int *count, int color_marker, int y_len, int x_len) {
+    void count_points(const int *image, int *count, const int color_marker, const int y_len, const int x_len) {
         int idx = blockIdx.x * blockDim.x + threadIdx.x;
         if (idx < y_len * x_len) {
             int x = idx % x_len;
@@ -125,7 +125,7 @@ def compute_kn_distances(points, k):
 
     kernel_code =  r'''
     extern "C" __global__
-    void compute_kn_distances(const int *points, double *kn_distances, int num_points, int k) {
+    void compute_kn_distances(const int *points, double *kn_distances, const int num_points, const int k) {
         int idx = blockIdx.x * blockDim.x + threadIdx.x;
         if (idx < num_points) {
             int x1 = points[idx * 2];
@@ -166,6 +166,33 @@ def compute_kn_distances(points, k):
     compute_kn_distances_kernel((blocks_per_grid,), (threads_per_block,), (points, kn_distances, num_points, k))
     return kn_distances
 
+# def dbscan(points, eps, min_pts):
+#     labels = np.zeros(len(points), dtype=np.int64) - 1
+#     labels, cluster_count= dbscan_core(points, eps, min_pts, labels)
+#     return labels,cluster_count
+
+# def dbscan_core(points, eps, min_pts, labels):
+#     cluster_id = cp.zeros(1, dtype=cp.int32)
+#     num_points = len(points) // 2
+    
+#     threads_per_block = 256
+#     blocks_per_grid = (num_points + (threads_per_block - 1)) // threads_per_block
+#     kernel_code = r'''
+#     extern "C" __global__
+#     void dbscan_kernel(const int *points, const int num_points, const float eps, const int min_pts, int *labels, int *cluster_id) {
+#         int idx = blockIdx.x * blockDim.x + threadIdx.x;
+#         if (idx < num_points) {
+#             // Implement DBSCAN logic here
+#         }
+#     }
+#     '''
+
+#     module = cp.RawModule(code=kernel_code)
+#     dbscan_kernel = module.get_function('dbscan_kernel')
+#     dbscan_kernel((blocks_per_grid,), (threads_per_block,), (points, num_points, eps, min_pts, labels, cluster_id))
+#     return label, int(cluster_id.get()[0])
+    
+
 if __name__ == "__main__":
     # Start timing
     start = time.time()
@@ -181,11 +208,14 @@ if __name__ == "__main__":
     # Extract points from the image
     points = get_points_in_cluster(image_bw, color_marker)
     points_cpu = cp.asnumpy(points)
-    print(f"Number of points to cluster: {len(points_cpu)/2}")
+    print(f"Number of points to cluster: {len(points_cpu)//2}")
     
     eps = get_epsilon(points, k=5,std_scale=std_scale)
     timeEpsilon = time.time() # Time after epsilon calculation
     print("TimeEpsilon = ", timeEpsilon - start)
+    
+    # labels,cluster_count = dbscan(points, eps, min_pts=5)
+    # print(f"Number of clusters found: {cluster_count}")
     
     
     
